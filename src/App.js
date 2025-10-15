61bcee6f0d3791334c4ee4f7c29c6c54c1a49e03
siamcats <siamomoo@gmail.com>
2025-10-15 17:20:31 +0900

    Fix white screen issue: use HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { GameProvider } from './contexts/GameContext';
 import { SettingsProvider } from './contexts/SettingsContext';
 import { StatsProvider } from './contexts/StatsContext';
